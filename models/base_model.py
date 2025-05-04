ff0c9afc8167f6ad4c77f50bd7058de19bd75ed7
edward <84643074+edwardsila@users.noreply.github.com>
2025-05-04 14:33:37 +0300

    update base model

--- models/base_model.py
+++ models/base_model.py
@@ -8,9 +8,10 @@ from datetime import datetime
 
 class BaseModel:
     def __init__(self):
-        self.id = str(uuid.uuid4()) ''' generates a unique id as string'''
-        self.created_at = datetime()
-        self.updated_at = datetime()
+        ''' generates a unique id as string'''
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         ''' Useful for debugging: shows class name, ID, and all attributes
@@ -23,7 +24,7 @@ class BaseModel:
         datetime'''
         self.updated_at = datetime.now()
 
-    def to_dict(self, __dict__):
+    def to_dict(self):
         ''' return a dictionary containing all key/values of __dict__
         on the instance:'''
         return {
