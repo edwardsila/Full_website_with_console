f32853c3637d023993d34d60085c25ffc349def2
edward <84643074+edwardsila@users.noreply.github.com>
2025-05-04 14:21:58 +0300

    base model

---
+++ models/base_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
+''' a class base model that defines all
+    common attributes/ methods for other classes
+'''
+
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4()) ''' generates a unique id as string'''
+        self.created_at = datetime()
+        self.updated_at = datetime()
+
+    def __str__(self):
+        ''' Useful for debugging: shows class name, ID, and all attributes
+        (like a snapshot of the object). '''
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        ''' public instance method that updates the
+        public instance attribute update_at with the current
+        datetime'''
+        self.updated_at = datetime.now()
+
+    def to_dict(self, __dict__):
+        ''' return a dictionary containing all key/values of __dict__
+        on the instance:'''
+        return {
+            "__class__": self.__class__.__name__,
+            "id": self.id,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat(),
+            **{k: v for k, v in self.__dict__.items()
+                if k not in ['created_at', 'updated_at']}
+        }
